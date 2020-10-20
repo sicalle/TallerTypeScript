4bc4c420236c0dd3df6ba320974a4900fc2a7195
Sara Calle <si.calle@uniandes.edu.co>
2020-10-20 17:48:27 -0500

    Taller TypeScript

---
+++ dataCourses.ts
@@ -0,0 +1,16 @@
+
+import { Course } from './course.js';
+
+export const dataCourses = [
+  new Course("Ingeniería de Sw", "Jose Bocanegra", 3),
+  new Course("Dinamica de sistemas", "Laura Guzman", 3),
+  new Course("Modelos probabilisticos", "Samuel Rodriguez", 3),
+  new Course("Control de producción", "Sepideh abogard", 3),
+  new Course("Sistemas transaccionales", "Claudia Jimenez", 3),
+  new Course("Anadec", "Miguel Angel", 3), 
+  new Course("TI en las organizaciones", "Ricardo Gomez", 3)
+]
+
+	
+	
+	
